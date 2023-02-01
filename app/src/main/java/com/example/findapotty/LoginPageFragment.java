8e4c1077c6fa27f295a680603f0fa033107fbbad
kaylynnkk <kaylynn.khem@student.csulb.edu>
2023-01-31 21:16:29 -0800

    login button navigates to login page

--- app/src/main/java/com/example/findapotty/LoginPageFragment.java
+++ app/src/main/java/com/example/findapotty/LoginPageFragment.java
@@ -11,15 +11,15 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
-public class LoginFragment extends Fragment {
+public class LoginPageFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_login_page, container, false);
 
         rootView.findViewById(R.id.fl_login_in_button).setOnClickListener(view -> {
             NavController controller = Navigation.findNavController(view);
-            controller.navigate(R.id.action_loginFragment2_to_login_page);
+            controller.navigate(R.id.action_loginFragment2_to_nav_search);
         });
 
         return rootView;

60d8ffcfc95a5143d7c0ea3720d8d9aca7049f7b
Sara Hamidi <103468494+saraHamidiii@users.noreply.github.com>
2023-02-05 11:47:12 -0800

    Sign up fragment
    I accidentally may have messed up the sign up and this is my attempt at fixing it

--- app/src/main/java/com/example/findapotty/signUpFragment.java
+++ app/src/main/java/com/example/findapotty/signUpFragment.java
@@ -10,24 +10,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-
-<<<<<<< Updated upstream
-public class SignUpFragment extends Fragment {
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_signup, container, false);
-
-        // Need to create 'signup' button to navigate to signup page
-        //random comment
-        rootView.findViewById(R.id.fl_login_in_button).setOnClickListener(view -> {
-            NavController controller = Navigation.findNavController(view);
-            controller.navigate(R.id.action_loginFragment2_to_nav_search);
-        });
-
-        return rootView;
-=======
-    public class signUpFragment extends Fragment {
+    public class SignUpFragment extends Fragment {
 
         @Nullable
         @Override
@@ -42,7 +25,7 @@ public class SignUpFragment extends Fragment {
 
             return rootView;
         }
->>>>>>> Stashed changes
+
     }
 
 

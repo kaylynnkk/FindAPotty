f05c42893aa506bef8009e536bd69bc925cc9f5c
Sara Hamidi <103468494+saraHamidiii@users.noreply.github.com>
2023-02-10 20:36:36 -0800

    New Register and Login Classes

--- app/src/main/java/com/example/findapotty/Login.java
+++ app/src/main/java/com/example/findapotty/Login.java
@@ -1,7 +1,11 @@
 package com.example.findapotty;
 
+
+
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,6 +18,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class Login extends AppCompatActivity {
 
@@ -21,12 +27,14 @@ public class Login extends AppCompatActivity {
     EditText inputUsername, inputPassword;
     Button fl_login_button;
     private FirebaseAuth mAuth;
+    //can remove line below
+    private static final String TAG = "EmailPassword";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_login);
+        setContentView(R.layout.activity_login);
 
         fl_login_button = findViewById(R.id.fl_signup_button);
         inputUsername = findViewById(R.id.inputUsername);
@@ -35,20 +43,39 @@ public class Login extends AppCompatActivity {
         fl_login_button.setOnClickListener((v)->{checkCredentials();});
         mAuth = FirebaseAuth.getInstance();
 
-        btn.setOnClickListener((v)->{
-            startActivity(new Intent(Login.this, Register.class));
-        });
+//        btn.setOnClickListener((v)->{
+//            startActivity(new Intent(loginActivity.this, loginActivity.class));
+//        });
 
     }
+
 
     private void checkCredentials()
     {
         String username = inputUsername.getText().toString();
         String password = inputPassword.getText().toString();
 
+        mAuth.signInWithEmailAndPassword(username, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
         if(username.isEmpty() || !(username.length() > 7))
         {
             showError(inputUsername, "Your username is not valid!");
+
         }
         else if(password.isEmpty() || password.length()<7)
         {
@@ -64,9 +91,10 @@ public class Login extends AppCompatActivity {
                     //made second parameter MapFragment to navigate to that page
                     if(task.isSuccessful())
                     {
-                        Intent intent = new Intent(Login.this, MapFragment.class);
-                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_NEW_TASK);
-                        startActivity(intent);
+//                        Intent intent = new Intent(Login.this, MapFragment.class);
+//                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_NEW_TASK);
+//                        startActivity(intent);
+                        Toast.makeText(Login.this, "Successful Login", Toast.LENGTH_SHORT).show();
                     }
                     else
                     {
--- app/src/main/java/com/example/findapotty/Register.java
+++ app/src/main/java/com/example/findapotty/Register.java
@@ -15,9 +15,9 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-import com.example.findapotty.databinding.ActivityRegisterBinding;
 
+
+//import com.example.findapotty.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -28,7 +28,7 @@ public class Register extends AppCompatActivity {
     TextView btn;
 
     private EditText inputUsername, inputPassword, inputEmail, confirmPassword;
-    Button fl_register_button;
+    Button fl_register_button, fl_account_button;
     private FirebaseAuth mAuth;
     private ProgressBar mLoadingBar;
 
@@ -59,6 +59,7 @@ public class Register extends AppCompatActivity {
 
 
         fl_register_button = findViewById(R.id.fl_register_button);
+        fl_account_button = findViewById(R.id.fl_account_button);
         //fl_register_button.setOnClickListener((v) -> {checkCredentials();});
         fl_register_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,58 +68,61 @@ public class Register extends AppCompatActivity {
             }
         });
 
-        btn.setOnClickListener((v) -> {
-            startActivity(new Intent(Register.this, Login.class));
+        fl_account_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Register.this, Login.class);
+                startActivity(intent);
+            }
         });
+
+
     }
 
 
-    private void showError(EditText input, String s)
-    {
+    //was commented out
+//        btn.setOnClickListener((v) -> {
+//            startActivity(new Intent(MainActivity.this, MainActivity.class));
+//       });
+    //}
+
+
+    private void showError(EditText input, String s) {
         input.setError(s);
         input.requestFocus();
     }
+
     private void checkCredentials() {
         String username = inputUsername.getText().toString();
         String email = inputEmail.getText().toString();
         String password = inputPassword.getText().toString();
         String confirmPass = confirmPassword.getText().toString();
 
-        if(username.isEmpty() || username.length() < 7)
-        {
+        if (username.isEmpty() || username.length() < 7) {
             showError(inputUsername, "Your username is not valid!");
-        }
-        else if(email.isEmpty() || email.contains("@"))
-        {
+        } else if (email.isEmpty() || !email.contains("@")) {
             showError(inputEmail, "Email is not valid!");
-        }
-        else if(password.isEmpty() || password.length()<7)
-        {
+        } else if (password.isEmpty() || password.length() < 7) {
             showError(inputPassword, "Password must be at least 7 characters long!");
-        }
-        else if(confirmPass.isEmpty() || confirmPass.equals(password))
-        {
+        } else if (confirmPass.isEmpty() || !confirmPass.equals(password)) {
             showError(confirmPassword, "Password does not match!");
-        }
-        else
-        {
+        } else {
             //progress bar stuff would go here
             //Toast.makeText(this, "Call Registration method", Toast.LENGTH_SHORT).show();
 
-            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
 
-                    if(task.isSuccessful())
-                    {
+                    if (task.isSuccessful()) {
                         Toast.makeText(Register.this, "Successful Registration", Toast.LENGTH_SHORT).show();
 
                         //changed MainActivity.class in second parameter to Login.class
+                        //was commented out
                         Intent intent = new Intent(Register.this, Login.class);
-                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_NEW_TASK);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
                         startActivity(intent);
-                    }
-                    else{
+                    } else {
 
                         Toast.makeText(Register.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
                     }
@@ -127,5 +131,5 @@ public class Register extends AppCompatActivity {
             });
         }
     }
-
 }
+

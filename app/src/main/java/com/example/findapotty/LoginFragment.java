fa67354f80271d5bb8ce62b47f710bc0aaf97d80
marmot <78244769+DerivativeMarmot@users.noreply.github.com>
2023-02-17 16:25:22 -0800

    manually merged login and signup page

--- app/src/main/java/com/example/findapotty/LoginFragment.java
+++ app/src/main/java/com/example/findapotty/LoginFragment.java
@@ -1,9 +1,12 @@
 package com.example.findapotty;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,39 +16,86 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import com.example.findapotty.databinding.FragmentLoginBinding;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginFragment extends Fragment {
+
+    private static final String TAG = "LoginFragment";
+    private FirebaseAuth mAuth;
+    private FragmentLoginBinding binding;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        FragmentLoginBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false);
         binding.setLifecycleOwner(this);
 
-        // login
-        binding.flLoginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                DatabaseReference mdb = FirebaseDatabase.getInstance().getReference();
-                User user = new User("ano", "100");
-                mdb.child("users").child(mdb.push().getKey()).setValue(user);
+        mAuth = FirebaseAuth.getInstance();
 
-                NavController controller = Navigation.findNavController(view);
-                controller.navigate(R.id.action_loginFragment2_to_nav_search);
-            }
+        binding.flLoginButton.setOnClickListener(view -> {
+            checkCredentials(view);
         });
-
-        // sign up
-        binding.flSignupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                NavController controller = Navigation.findNavController(view);
-                controller.navigate(R.id.action_navg_login_fragment_to_nav_signup_fragment);
-            }
+        binding.flSignupButton.setOnClickListener(view -> {
+            actionSignUpPage(view);
         });
 
         return binding.getRoot();
+    }
+
+    private void actionSignUpPage(View view) {
+        NavController controller = Navigation.findNavController(view);
+        controller.navigate(R.id.action_navg_login_fragment_to_nav_signup_fragment);
+    }
+
+//    private void loginListener() {
+//        binding.flLoginButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                DatabaseReference mdb = FirebaseDatabase.getInstance().getReference();
+//                User user = new User("ano", "100");
+//                mdb.child("users").child(mdb.push().getKey()).setValue(user);
+//
+//                NavController controller = Navigation.findNavController(view);
+//                controller.navigate(R.id.action_loginFragment2_to_nav_search);
+//            }
+//        });
+//    }
+
+    private void checkCredentials(View view) {
+        String username = binding.loginInputUsername.getText().toString();
+        String password = binding.loginInputPassword.getText().toString();
+
+
+        if (username.isEmpty() || !(username.length() > 7)) {
+            showError(binding.loginInputUsername, "Your username is not valid!");
+
+        } else if (password.isEmpty() || password.length() < 1) {
+            showError(binding.loginInputPassword, "Password must be at least 7 characters!");
+
+        } else {
+            mAuth.signInWithEmailAndPassword(username, password)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            Toast.makeText(binding.getRoot().getContext(), "Authentication successful.",
+                                    Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            NavController controller = Navigation.findNavController(view);
+                            controller.navigate(R.id.action_loginFragment2_to_nav_search);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(view.getContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+    }
+
+    private void showError(EditText input, String s) {
+        input.setError(s);
+        input.requestFocus();
     }
 }
--- app/src/main/java/com/example/findapotty/SignUpFragment.java
+++ app/src/main/java/com/example/findapotty/SignUpFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,14 +15,65 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import com.example.findapotty.databinding.FragmentSignupBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpFragment extends Fragment {
+
+    private static final String TAG = "LoginFragment";
+    private FirebaseAuth mAuth;
+    private FragmentSignupBinding binding;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        FragmentSignupBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_signup, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_signup, container, false);
         binding.setLifecycleOwner(this);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        binding.flRegisterButton.setOnClickListener(view -> {
+            checkCredentials(view);
+        });
+
         return binding.getRoot();
+    }
+
+
+    private void checkCredentials(View view) {
+        String username = binding.signupInputUsername.getText().toString();
+        String email = binding.inputEmail.getText().toString();
+        String password = binding.signupInputPassword.getText().toString();
+        String confirmPass = binding.confirmPassword.getText().toString();
+
+        if (username.isEmpty() || username.length() < 7) {
+            showError(binding.signupInputUsername, "Your username is not valid!");
+        } else if (email.isEmpty() || !email.contains("@")) {
+            showError(binding.inputEmail, "Email is not valid!");
+        } else if (password.isEmpty() || password.length() < 7) {
+            showError(binding.signupInputPassword, "Password must be at least 7 characters long!");
+        } else if (confirmPass.isEmpty() || !confirmPass.equals(password)) {
+            showError(binding.confirmPassword, "Password does not match!");
+        } else {
+
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+
+                if (task.isSuccessful()) {
+                    Toast.makeText(view.getContext(), "Successful Registration", Toast.LENGTH_SHORT).show();
+                    // back to login
+                    NavController controller = Navigation.findNavController(view);
+                    controller.navigate(R.id.action_nav_signup_fragment_to_navg_login_fragment);
+
+                } else {
+//                    Toast.makeText(view.getContext(), task.getException().toString(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(view.getContext(), "Specified email already exists, please use another one",
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    private void showError(EditText input, String s) {
+        input.setError(s);
+        input.requestFocus();
     }
 }

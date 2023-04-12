26107878642f394d8aac3aef1ba22fbe7197a6cd
kaylynnkk <kaylynnkhem@gmail.com>
2023-04-11 17:18:20 -0700

    Update FeedFragment.java

--- app/src/main/java/com/example/findapotty/feed/FeedFragment.java
+++ app/src/main/java/com/example/findapotty/feed/FeedFragment.java
@@ -26,6 +26,8 @@ public class FeedFragment extends Fragment{
         // Create list of developers
         List<Article> articleList = new ArrayList<>();
         articleList.add(new Article("Michelle Lewis","Wind and solar achieve a record high of 12% of global electricity in 2022","Wind and solar reached a record 12% of global electricity in 2022, up from 10% in 2021, according to a report launched today by energy think tank Ember – and experts predict that power sector emissions have peaked.",R.drawable.icon1));
+        articleList.add(new Article("thestreet.com","Cathie Wood Bets Big on a Video Game Company With an Exciting Future","The fund also cut its shares in a popular tech giant. “Out with the old and in with the new” seems to be Ark Investment Management’s strategy as of late. This makes complete sense--the fund is the project of well-known stock market pundit and investor Cathie …",R.drawable.icon1));
+        articleList.add(new Article("Alex Misoyannis","BYD Dolphin electric-car arrival pushed back, due later this year alongside Seal sedan","Chinese electric-car giant BYD's next two models – the Dolphin and Seal – are due closer to the end of this year than previously forecast.",R.drawable.icon1));
 
         // Create a new articleadapter with the list of developers
         ArticleAdapter articleAdapter = new ArticleAdapter(articleList);

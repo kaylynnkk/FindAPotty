c900c8bf15cc085630894f1ee5e1961da690724a
melissajg <42328365+melissajg@users.noreply.github.com>
2023-05-12 20:33:59 -0700

    Update RestroomReviewRecyclerViewAdaptor.java

--- app/src/main/java/com/example/findapotty/search/restroompage/RestroomReviewRecyclerViewAdaptor.java
+++ app/src/main/java/com/example/findapotty/search/restroompage/RestroomReviewRecyclerViewAdaptor.java
@@ -77,7 +77,7 @@ public class RestroomReviewRecyclerViewAdaptor extends RecyclerView.Adapter<Rest
                     holder.helpfulnessBT.setImageResource(R.drawable.baseline_thumb_up_off_alt_24);
                     currHelpVal = reviewObj.getHelpfulness()-1;
                     reviewObj.setHelpfulness(currHelpVal);
-                    holder.helpfulnessTV.setText("Helpfulness ("+currHelpVal+")");
+                    holder.helpfulnessTV.setText("Helpfulness ("+Math.abs(currHelpVal)+")");
                     dbr.child(key).child("helpfulness").setValue(currHelpVal);
                     previouslyClicked = false;
 
